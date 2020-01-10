1eaca606d393d5443a6c3fcb13cdb53acc8ecf69
kottetall <taikes_bass@hotmail.com>
2020-01-10 15:46:31 +0100

    +räknare & lite css. Finns buggar i räknaren som måste fixas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-function gameLoop() {
-    //
-}
+window.onload = andraNummer()
 
 function uppdateraPoang(nytt) {
     //TODO: kolla smidigare sätt att hämta poäng som int istället för sträng
     document.querySelector(".score").textContent = parseInt(document.querySelector(".score").textContent) + nytt
 }
 
-function slumpNummer(max = 10) {
+function slumpNummer(max = 100) {
     //max - maxtaket som spelaren väljer, ex 5 ger siffror mellan 0-5
     return Math.floor(Math.random() * max)
 }
@@ -31,18 +29,51 @@ function kontroll(e) {
 
 function rattSvar() {
     uppdateraPoang(1)
+    // andraNummer()
     alert("rätt") //FIXME: Ta bort alerter! TILLFÄLLIG LÖSNING
+    // raknaNed()
+    gameLoop()
 }
 
 function felSvar() {
-    uppdateraPoang(-1)
+    // uppdateraPoang(-1)
     alert("fel") //FIXME: Ta bort alerter! TILLFÄLLIG LÖSNING
+    gameLoop()
+}
+
+function andraNummer() {
+    document.querySelector(".number").textContent = slumpNummer(document.querySelector(".randomLimit").value)
 }
 
 document.querySelectorAll(".button").forEach((button) => {
     button.addEventListener("click", kontroll)
 })
 
-document.querySelector(".slumpNummer").addEventListener("click", () => {
-    document.querySelector(".number").textContent = slumpNummer()
-})
+
+
+function raknaNed() {
+    //TODO: gör mer flytande - ex intervall för 100ms och var 10de cykel ändras sekundvisaren
+    //FIXME: räknaren ökar när man trycker på en gissning - pga att man aldrig tar bort den ursprungliga intervallen. Ev lösa genom att knapptryck lägger till data-pressed=true och lägger det som ett vilkor som hämtas varje gång.
+    counter = startCount = document.querySelector(".timeLimit").value
+    document.querySelector(".countDown").textContent = counter + "sec"
+    document.querySelector(".progressIn").style.width = "100%"
+    counting = setInterval(() => {
+        if (counter === 0) {
+            clearInterval(counting)
+            felSvar()
+        } else {
+            counter--
+            document.querySelector(".countDown").textContent = counter + "sec"
+            document.querySelector(".progressIn").style.width = Math.round((counter / startCount) * 100) + "%"
+
+            document.querySelectorAll(".button").forEach((button) => {
+                button.addEventListener("click", kontroll)
+            })
+        }
+    }, 1000)
+}
+
+function gameLoop() {
+    andraNummer()
+    raknaNed()
+}
